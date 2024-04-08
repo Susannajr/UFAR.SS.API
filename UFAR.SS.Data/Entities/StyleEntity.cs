a1a2d443cbefa9a0fd2697d6ea487e656e4d806f
tyom29 <artyomyeprikyan@outlook.com>
2024-04-08 17:59:13 +0400

    Fixes

--- UFAR.SS.Data/Entities/StyleEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace UFAR.SS.Data.Entities
-{
-    public class StyleEntity : BaseEntity
-    {
-        public string Name { get; set; }
-    }
-}
